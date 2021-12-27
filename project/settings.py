35f0661599754c056f5c632a39094f2d256b04c1
Jahidul Alam <mjahidulalam@gmail.com>
2021-12-26 17:48:46 -0700

    updated static

--- project/settings.py
+++ project/settings.py
@@ -169,7 +169,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_ROOT  = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 # Default primary key field type
